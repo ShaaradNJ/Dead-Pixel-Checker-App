fc972ceb522dde66873b9192a0557e792930afea
shaaradnj <shaaradjarandikar@gmail.com>
2024-06-05 13:28:36 +0530

    created home_screen.dart file...

---
+++ lib/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Home_screen extends StatelessWidget {
+  const Home_screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
