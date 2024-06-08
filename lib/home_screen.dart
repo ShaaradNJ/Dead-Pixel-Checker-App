118136f1e5641bbcabebb5d2ac4f23707af1aed2
shaaradnj <shaaradjarandikar@gmail.com>
2024-06-08 20:14:10 +0530

    created a block on home_screen.dart...

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,20 +1,42 @@
 import 'package:flutter/material.dart';
 
-class Home_screen extends StatelessWidget {
-  const Home_screen({super.key});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
       appBar: AppBar(
         backgroundColor: Colors.black,
         title: const Center(child: Text("Dead-pixel-checker",)),
-        foregroundColor: Colors.white,
+        foregroundColor: Color.fromARGB(255, 255, 255, 255),
         titleTextStyle: const TextStyle(fontSize: 24),
 
         
       ),
+      body: 
+      Column(
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(10,70,10,10),
+              child: Container(
+                height: 50,
+                width: 280,
+                decoration:BoxDecoration(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(30),
+
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+      
+      
 
     );
   }
