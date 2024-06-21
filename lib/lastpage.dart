97aa7e55c8f4aeb55a56e208bb2dc43a9782484b
shaaradnj <shaaradjarandikar@gmail.com>
2024-06-21 21:37:35 +0530

    last page.dart...

---
+++ lib/lastpage.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+class LastPage extends StatelessWidget {
+  const LastPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Center(
+          child:  Text("Dead-pixel-Detector",
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.white,
+          ),),
+        ),
+      ),
+      body: 
+      Container(
+        
+      ),
+      
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:dead_pixel_checker_app/home_screen.dart';
 import 'package:dead_pixel_checker_app/info_dead_pixels.dart';
 import 'package:dead_pixel_checker_app/instruction.dart';
 import 'package:dead_pixel_checker_app/instruction2.dart';
+import 'package:dead_pixel_checker_app/lastpage.dart';
 import 'package:dead_pixel_checker_app/login_page.dart';
 import 'package:dead_pixel_checker_app/red_screen.dart';
 import 'package:flutter/material.dart';
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  const MaterialApp(
-      home:HomeScreen(),
+      home:LastPage(),
       
       
 
